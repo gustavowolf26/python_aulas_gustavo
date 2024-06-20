785af9deabb4bb1a75341bacec110864d1b1d881
gustavowolf26 <gustavosimoes2606@gmail.com>
2024-06-19 22:34:27 -0300

    arquivo 19 adicionado

---
+++ Exercicios/19_idade.py
@@ -0,0 +1,31 @@
+#==============================================================================================================
+#Autor: Gustavo Simoes 
+#Versao:  1.0
+#Data: 13/06/2024
+#Algoritmo: 
+#Descrição: Escreva um algoritmo que leia a data de nascimento da pessoa e calcule a idade
+#           
+#           
+#           
+#           
+#           
+#===============================================================================================================
+
+#Inicio
+
+#Variáveis
+nascimento = 0
+idade = 0
+ano_atual = 2024
+
+nascimento = int(input('digite seu ano de nascimento: '))
+idade = ano_atual - nascimento
+
+print(idade)
+
+
+
+
+
+
+
