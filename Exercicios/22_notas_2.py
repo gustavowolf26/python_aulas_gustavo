1e250939a8dbd5e4bcec71bd70e24cb8faf5810b
gustavowolf26 <gustavosimoes2606@gmail.com>
2024-06-27 22:18:17 -0300

    Add files via upload

--- Exercicios/22_notas_2.py
+++ Exercicios/22_notas_2.py
@@ -18,16 +18,19 @@ jogador2 = ''
 rodadas = 0
 jogador = 'X'
 
-# jogador1 = input('Insira o nome do jogador 1: ')
-# print('jogador 1 é o x e inicia a rodada')
-# jogador2 = input('Insira o nome do jogador 2: ')
-# print('jogador 2 é o bolinha')
+jogador1 = input('Insira o nome do jogador 1: ')
+print('jogador 1 é o x e inicia a rodada')
+jogador2 = input('Insira o nome do jogador 2: ')
+print('jogador 2 é o bolinha')
 # nome = jogador1
 
 tabuleiro =[['1','2','3'],
             ['4','5','6'],
             ['7','8','9']
 ]
+
+def mostrar_vencedor(vencedor):
+    print('temos um vencedor', vencedor) 
 
 while rodadas < 10:
     
@@ -37,13 +40,17 @@ while rodadas < 10:
     for linha in range(3):
         linha_completa = ''
 
+    
+
         for coluna in range(3):
             if posicao == tabuleiro[linha][coluna]:
                 tabuleiro[linha][coluna] = jogador
                 posicao_encontrada = True       
             linha_completa += (f'| {tabuleiro[linha][coluna]} | ')
-        print(linha_completa)
+            
 
+        print(linha_completa)
+            
     if posicao_encontrada == True:
         rodadas += 1
         if jogador == 'X':
@@ -54,5 +61,25 @@ while rodadas < 10:
            
     else:
         print('posiçao nao encontrada')
-        
+
+    if (tabuleiro[0][0] == tabuleiro[0][1] == tabuleiro[0][2] or 
+        tabuleiro[1][0] == tabuleiro[1][1] == tabuleiro[1][2] or 
+        tabuleiro[2][0] == tabuleiro[2][1] == tabuleiro[2][2] or  
+        tabuleiro[0][0] == tabuleiro[1][0] == tabuleiro[2][0] or 
+        tabuleiro[0][1] == tabuleiro[1][1] == tabuleiro[2][1] or 
+        tabuleiro[0][2] == tabuleiro[1][2] == tabuleiro[2][2] or 
+        tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] or 
+        tabuleiro[2][0] == tabuleiro[1][1] == tabuleiro[2][0]):
+        mostrar_vencedor(jogador)
+
+        if jogador == 'O':
+            print( 'o vencedor é', jogador1)
+        else:
+            print('o vencedor é', jogador2)
+
+        break
+
+
+
+
 
